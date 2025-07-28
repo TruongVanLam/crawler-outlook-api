35e0ee2680a9d9806cf176e48cb665d6703d0dbd
truongvanlam <truongvanlam176@gmail.com>
2025-07-28 21:12:11 +0700

    all origin

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://outlook-mail.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
