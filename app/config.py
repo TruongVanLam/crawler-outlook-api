6b304ec2087357d3355ca72fa76aa46273a19590
TranDoan02 <trandoan020202@gmail.com>
2025-08-04 15:35:52 +0700

    update

--- app/config.py
+++ app/config.py
@@ -7,7 +7,8 @@ from typing import List
 # Microsoft Graph API Configuration
 CLIENT_ID = "b6239e39-c5f9-4704-ac0d-bcb0e0dc87b6"
 CLIENT_SECRET = "cOb8Q~KsEr2B.UpGCBxp5Sqcs6JnBs~Osc_~fa4B"
-REDIRECT_URI = "http://localhost:8000/auth/callback"
+# REDIRECT_URI = "http://localhost:8000/auth/callback"
+REDIRECT_URI = "https://exec-reached-boston-alice.trycloudflare.com/api/v1/auth/callback"
 AUTHORITY = "https://login.microsoftonline.com/consumers"  # Cho tài khoản cá nhân
 SCOPE: List[str] = ["offline_access", "Mail.Read"]  # offline_access để lấy refresh_token
 GRAPH_API_BASE = "https://graph.microsoft.com/v1.0"
